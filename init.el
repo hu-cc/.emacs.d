a38a9842ba198547f42971c37914fe00d6dcd870
hu-cc <huccyuyue@163.com>
2021-12-08 16:29:23 +0800

    Add magit

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; user Melpa-Stable
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+
 ;; Preferences
 (setq inhibit-startup-screen t)
 
@@ -22,3 +28,17 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
