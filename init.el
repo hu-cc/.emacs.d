4a61f00849372423bed488796daeefb8e3a7aac9
hu-cc <huccyuyue@163.com>
2021-12-11 13:49:04 +0800

    Add use-package, and configuration is written in org-mode

--- init.el
+++ init.el
@@ -1,47 +1,26 @@
-;; user Melpa-Stable
+;; Require and initialize `package`.
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
 
+;; Add `melpa` to `package-archives`.
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
-;; Preferences
-(prefer-coding-system 'utf-8)
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(setq inhibit-startup-screen t)
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(global-hl-line-mode 1)
-
-(setq ring-bell-function 'ignore)
-
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t    ; Don't delink hardlinks
-      version-control t      ; Use version numbers on backups
-      delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 20   ; how many of the newest versions to keep
-      kept-old-versions 5    ; and how many of the old
-      )
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Appearance
-
-(set-frame-font "Fira Code Retina 12" nil t)
-
-(add-hook 'emacs-startup-hook 'toggle-frame-maximized)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn t)
-
-
+;; TODO: use Org manage config
+;; The actual config file
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
