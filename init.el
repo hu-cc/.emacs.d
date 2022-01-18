44e2ca2c2ffec05cc9656b4f334c046ff552f01c
hu-cc <huccyuyue@163.com>
2022-01-19 01:38:43 +0800

    font, slime

--- init.el
+++ init.el
@@ -14,12 +14,13 @@
 ;; The actual config file
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme use-package)))
+ '(package-selected-packages '(slime-volleyball zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
