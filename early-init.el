d29c897a2ca1568611c6328daa6e665702f3c936
hu-cc <huccyuyue@163.com>
2022-04-21 13:02:37 +0800

    refer cabins's emacs config

---
+++ early-init.el
@@ -0,0 +1,24 @@
+;; For speed up the startup, please do NOT forget reset it to default
+;; after Emacs after-init-hook, or it may cause freezes.
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
+
+;; Prevent unwanted runtime compilation for native-comp users
+(setq native-comp-deferred-compilation nil)
+
+;; Package initialize occurs automatically, before `user-init-file' is loaded
+;; but after `early-init-file'. If you want to handle package initialization,
+;; you can prevent Emacs from doing it early by uncomment next line!
+(setq package-enable-at-startup nil)
+
+;; [From DOOM Emacs]
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize t)
+
+;; System default coding
+(set-language-environment 'utf-8)
--- init.el
+++ init.el
@@ -1,15 +1,24 @@
 ;; Require and initialize `package`.
 (require 'package)
-(package-initialize)
+(setq package-check-signature nil
+      load-prefer-newer t)
 
-;; Add `melpa` to `package-archives`.
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
 
 ;; use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(require 'use-package)
+
+;; diminish & delight, as use-package optional dependency
+(use-package diminish)
+(use-package delight)
 
 ;; The actual config file
 (when (file-readable-p "~/.emacs.d/config.org")
@@ -20,7 +29,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(slime-volleyball zenburn-theme use-package)))
+ '(package-selected-packages
+   '(company dap-mode lsp-ui lsp-mode yaml-mode protobuf-mode markdown-mode json-mode typescript-mode web-mode emmet-mode go-mode eglot yasnippet highlight-parentheses rainbow-delimiters delight diminish which-key popwin info-colors crux ctrlf use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
