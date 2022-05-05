ba0293c1c3d736d52377c30ffda5693c49630815
hu-cc <huccyuyue@163.com>
2022-05-05 23:20:40 +0800

    Add org-roam

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company dap-mode lsp-ui lsp-mode yaml-mode protobuf-mode markdown-mode json-mode typescript-mode web-mode emmet-mode go-mode eglot yasnippet highlight-parentheses rainbow-delimiters delight diminish which-key popwin info-colors crux ctrlf use-package)))
+   '(org-roam company dap-mode lsp-ui lsp-mode yaml-mode protobuf-mode markdown-mode json-mode typescript-mode web-mode emmet-mode go-mode eglot yasnippet highlight-parentheses rainbow-delimiters delight diminish which-key popwin info-colors crux ctrlf use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
